dce505c664ac927ee0213b84bf90c21e54e70d2f
Deepti-Jana <deeptij.3690@gmail.com>
2026-03-23 23:12:58 +0530

    UC3: Display Hello with name or default World

--- Hello.java
+++ Hello.java
@@ -1,12 +1,16 @@
 /**
  * @author Deepti Jana
- * @version 2.0
+ * @version 3.0
  *@since UC1
  */
 
 public class Hello {
     public static void main(String[] args) {
-        String name= args[0];
-        System.out.println("Hello, " + name+ "!");        
+        String name="World"; //Default name
+
+        if (args.length > 0) {
+            name = args[0];
+        } 
+        System.out.println("Hello " + name+"!");
     }
 }
