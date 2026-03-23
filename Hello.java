d138cf797e28e142609a14fb76ff18cd69c6fad3
Deepti-Jana <deeptij.3690@gmail.com>
2026-03-23 23:06:59 +0530

    UC2: Display Hello with command-line argument

--- Hello.java
+++ Hello.java
@@ -1,10 +1,12 @@
 /**
  * @author Deepti Jana
- * @version 1.0
+ * @version 2.0
+ *@since UC1
  */
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String name= args[0];
+        System.out.println("Hello, " + name+ "!");        
     }
 }
